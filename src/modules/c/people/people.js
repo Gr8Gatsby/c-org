5e2913ff5cd3e05bee3e9ca3158c1eac4d4e8419
Kevin <kevinhill@live.com>
2019-09-12 13:46:06 -0400

    removed console.log

--- src/modules/c/people/people.js
+++ src/modules/c/people/people.js
@@ -2,8 +2,4 @@ import { LightningElement, api } from 'lwc';
 
 export default class People extends LightningElement {
     @api people;
-
-    connectedCallback() {
-        // window.console.log(this.people);
-    }
 }
