c6a6faf4c8c288f65da9a24dcd9512c917f6ec36
Nathan Totten <ntotten@salesforce.com>
2019-10-16 17:46:18 -0400

    Convert to sfdx app

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { buildCustomElementConstructor } from 'lwc';
-import MyApp from 'c/app';
-import MyOrg from 'c/org';
-import MyPerson from 'c/person';
-
-customElements.define('c-app', buildCustomElementConstructor(MyApp));
-customElements.define('c-org', buildCustomElementConstructor(MyOrg));
-customElements.define('c-person', buildCustomElementConstructor(MyPerson));
--- src/modules/c/people/people.js
+++
@@ -1,5 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class People extends LightningElement {
-    @api people;
-}
